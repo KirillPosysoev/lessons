bf8a8960ea5284a7d99271047fdbfa0a33fe2dd6
KirillPosysoev <posysaev.ka@gmail.com>
2023-12-08 18:08:45 +0300

    ДЗ4

---
+++ src/homework4/Customer.java
@@ -0,0 +1,8 @@
+package homework4;
+
+public class Customer {
+    private String firstName;
+    private String secondName;
+    private String middleName;
+    private Date birthDay;
+}
---
+++ src/homework4/Date.java
@@ -0,0 +1,8 @@
+package homework4;
+
+public class Date {
+    private int day;
+    private int month;
+    private int year;
+
+}
